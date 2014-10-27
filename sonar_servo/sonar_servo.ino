a23c2a6dc1b91bc5bf884b0b2847dc25dd71da10
alex <alexander.makrigiorgos@mail.mcgill.ca>
2014-10-27 16:32:01 -0400

    Version of code used in demo, launchcodes provided by ultrasonic Processing file.

--- sonar_servo/sonar_servo.ino
+++ sonar_servo/sonar_servo.ino
@@ -1,32 +1,21 @@
-#define ECHOPIN 4
-#define TRIGPIN 3
-#define LEDPIN 13
-#define EYESPIN 12
-#define VERPIN 11
-#define HORPIN 10
+#define VERPIN 12
+#define HORPIN 11
 
 #include <Servo.h>
 
-Servo eyes;
 Servo vertical;
 Servo horizontal;
-int pos = 0;
 int posVer, posHor;
-long duration, distance;
 
 void setup(){
   Serial.begin(9600);
-  pinMode(ECHOPIN, INPUT);
-  pinMode(TRIGPIN, OUTPUT);
-  pinMode(LEDPIN, OUTPUT);
-  eyes.attach(EYESPIN);
   vertical.attach(VERPIN);
   horizontal.attach(HORPIN);
 }
 
 String Print() {
   Serial.print(distance);
- // Serial.print("cm\n");
+  Serial.print("cm\n");
   Serial.print(",");
   Serial.print(pos);
   Serial.print(".\n");
@@ -34,71 +23,56 @@ String Print() {
 
 }
 
-float Distance(){
-  distance = 0;
-  for(int i=0; i<1; i++){
-    digitalWrite(TRIGPIN, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIGPIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIGPIN, LOW);
-    duration = pulseIn(ECHOPIN, HIGH);
-    distance += (duration/(2*29.1));
-  }
-  //distance = distance/1;
-  return distance;
-}
 
-void loop(){ 
-  Scan();
-  Serial.println(Aim());
-  Serial.println(Wait());
-  Serial.println(Fire());
+void loop(){
+    Serial.print(Aim());
+    Serial.print(Fire());
 }
 
 String Aim(){
- return ("Aiming...");
+  Serial.println("Aiming...");
+  vertical.write(posVer);
+  horizontal.write(posHor);
+  String launchCode = Wait();
+  if (launchCode == "154"){
+    posVer = 120;
+    posHor = 105;
+    System.out.println("Set switches to: ");
+  } else if (launchCode == "155"){
+    posVer = 130;
+    posHor = 105;
+    System.out.println("Set switches to: ");
+  } else if (launchCode == "156"){
+    posVer = 137;
+    posHor = 105;
+    System.out.println("Set switches to: ");
+  } else if (launchCode == "157"){
+    posVer = 145;
+    posHor = 105;
+    System.out.println("Set switches to: ");
+  } else {
+    posVer = 140;
+    posHor = 110;
+  }
+  return (" ");
+  Serial.println("Aiming...");
 }
 
 String Fire(){
- return "Fire!";
+ return " ";
 }
 
 String Wait(){
-  char FIRE;
+  int FIRE = -3;
   Serial.println("Waiting for command...");
-  while (FIRE != 'F'){
-    FIRE = Serial.read();
-    //horizontal.write(posHor);
-    //vertical.write(posVer);
+  while (FIRE == -3){
+    int byte1 = Serial.read();
+    int byte2 = Serial.read();
+    int byte3 = Serial.read();
+    FIRE = byte1 + byte2 + byte3;
+    horizontal.write(posHor);
+    vertical.write(posVer);
   }
-  return "Firing!";
+  return FIRE;
 }
 
-
-void Scan(){
-  Serial.println("Scanning.....");
-  eyes.write(75);
-  
-  for(int cycle = 0; cycle<3; cycle++){
-    for(pos = 75; pos < 105; pos+=3){
-      eyes.write(pos);
-      distance = Distance();
-      if(distance > 10){
-        Print();
-      }
-      delay(10);
-    }
-    delay(1000);
-    
-    for(pos = 105; pos > 75; pos-=3){
-      eyes.write(pos);
-      distance = Distance();
-      if(distance > 10){
-        Print();
-      }
-    delay(10);
-    }
-  }
-  delay(5000);
-}
